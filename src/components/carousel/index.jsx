7e388cb74f95af893e1a35e4ac080bb7eabc2489
Mauricio Gedrat <mauriciogedrat@gmail.com>
2021-10-22 14:06:59 -0300

    ok2

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -8,11 +8,11 @@ import {
     Stack,
 } from '@chakra-ui/react'
 import Image from 'next/image'
-import Carro1 from '../../assets/img/Ferrari.jpg'
-import Carro2 from '../../assets/img/Audi.jpg'
-import Carro3 from '../../assets/img/gtr.jpg'
-import Carro4 from '../../assets/img/Lambo-veneno.jpg'
-import Carro5 from '../../assets/img/porshe.jpg'
+import Carro1 from 'assets/img/Ferrari.jpg'
+import Carro2 from 'assets/img/Audi.jpg'
+import Carro3 from 'assets/img/gtr.jpg'
+import Carro4 from 'assets/img/Lambo-veneno.jpg'
+import Carro5 from 'assets/img/porshe.jpg'
 
 export default function Carousel() {
     const arrowStyles = {
